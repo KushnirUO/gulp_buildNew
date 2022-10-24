9ffb2ee65b203127b71276453103adfee221b63f
KushnirUO <kushnir1145@gmail.com>
2022-10-25 01:04:03 +0300

    update slider

--- src/scripts/develop/index.js
+++ src/scripts/develop/index.js
@@ -2,8 +2,48 @@
 
 
 $(document).ready(function(){
+    $('.insights-slider__wrapper').slick({
+        infinite: false,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true,
+        responsive: [
+            {
+                breakpoint: 1025,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+    $('.slide-prev').click(function(e){
+        //e.preventDefault();
+        $('.insights-slider__wrapper').slick('slickPrev');
+    } );
 
+    $('.slide-next').click(function(e){
+        //e.preventDefault();
+        $('.insights-slider__wrapper').slick('slickNext');
+    } );
 });
+
 
 $(window).load(function(){
 
