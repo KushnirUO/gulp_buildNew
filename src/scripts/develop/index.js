169f87f8f629b7e88632927e88d998d6bed6cc90
”Julia <kushnir1145@gmail.com>
2022-10-27 00:00:08 +0300

    add technology

--- src/scripts/develop/index.js
+++ src/scripts/develop/index.js
@@ -1,14 +1,42 @@
-
-
-
-$(document).ready(function(){
+$(document).ready(function () {
 
 });
 
-$(window).load(function(){
+$(window).load(function () {
 
 });
+$(window).scroll(function () {
+    var $height = $(window).scrollTop();
+    console.log($height);
+    if ($height > 900 && $height < 1200) {
+        $('.mobile-dev__img1').css('top', $height - 900);
+        $('.mobile-dev__img1').css('display','flex');
+        $('.mobile-dev__img2').css('display','none');
 
-$(window).resize(function(){
+    }
+    if ($height > 1200 && $height < 1500) {
+        $('.mobile-dev__img1').css('display','none');
+        $('.mobile-dev__img2').css('display','flex');
+        $('.mobile-dev__img2').css('top', $height - 900);
+        $('.mobile-dev__img3').css('display','none');
 
-});
+    }
+    if ($height > 1500 && $height < 1800) {
+        $('.mobile-dev__img2').css('display','none');
+        $('.mobile-dev__img3').css('display','flex');
+        $('.mobile-dev__img3').css('top', $height - 900);
+        $('.mobile-dev__img4').css('display','none');
+
+    }
+    if ($height > 1800 && $height < 2100) {
+        $('.mobile-dev__img3').css('display','none');
+        $('.mobile-dev__img4').css('display','flex');
+        $('.mobile-dev__img4').css('top', $height - 900);
+    }
+
+    }
+);
+
+    $(window).resize(function () {
+
+    });
